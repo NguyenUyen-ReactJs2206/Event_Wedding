6a46b9335854ded303e9b1c333d59114e1417fbe
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-11-24 00:23:06 +0700

    connect to mongo db

--- ServerEvent/src/index.ts
+++ ServerEvent/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import usersRouter from './routers/users.routes'
+import { run } from './services/database.services'
 const app = express()
 const router = express.Router()
 const port = 4000
@@ -11,6 +12,9 @@ app.get('/', (req, res) => {
 //Handle Json => {} when user post data
 app.use(express.json())
 app.use('/users', usersRouter)
+
+//Run MongoDB - error=>console.dir(error)
+run().catch(console.dir)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
