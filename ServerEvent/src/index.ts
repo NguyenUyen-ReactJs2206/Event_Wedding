afd8f5012fff75f235766dfea30e163d7f5eecec
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-11-24 00:52:48 +0700

    custom class function to connect mgDB

--- ServerEvent/src/index.ts
+++ ServerEvent/src/index.ts
@@ -1,8 +1,8 @@
 import express from 'express'
 import usersRouter from './routers/users.routes'
-import { run } from './services/database.services'
+import databaseService from './services/database.services'
+
 const app = express()
-const router = express.Router()
 const port = 4000
 
 app.get('/', (req, res) => {
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use('/users', usersRouter)
 
 //Run MongoDB - error=>console.dir(error)
-run().catch(console.dir)
+databaseService.connect()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- ServerEvent/src/services/database.services.ts
+++ ServerEvent/src/services/database.services.ts
@@ -1,15 +1,31 @@
-import { MongoClient, ServerApiVersion } from 'mongodb'
+import { Db, MongoClient } from 'mongodb'
+import { config } from 'dotenv'
+config()
 
-const uri = 'mongodb+srv://projectevent:projectevent@clusterevent.li65dq2.mongodb.net/?retryWrites=true&w=majority'
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri)
-export async function run() {
-  try {
-    // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
-    console.log('Pinged your deployment. You successfully connected to MongoDB!')
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close()
+console.log(process.env.DB_USERNAME)
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@clusterevent.li65dq2.mongodb.net/?retryWrites=true&w=majority`
+
+class DatabaseSevice {
+  private client: MongoClient
+  private db: Db
+  constructor() {
+    this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
+  }
+
+  async connect() {
+    try {
+      // Send a ping to confirm a successful connection
+      await this.db.command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } catch (error) {
+      console.log('Error', error)
+      throw error
+    }
   }
 }
+
+//Create Object from DatabaseService
+const databaseService = new DatabaseSevice()
+export default databaseService
